b9277d92cee2944f07cddb16abb7f44173e137b1
Kholin <oodzchen@gmail.com>
2024-11-10 20:06:36 +0800

    update: Log out

--- src/components/base/BNav.tsx
+++ src/components/base/BNav.tsx
@@ -9,6 +9,7 @@ import { isLogined, useAuthedUserStore } from '@/state/global'
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 import { Button } from '../ui/button'
 
+import { DropdownMenuItem } from '@radix-ui/react-dropdown-menu'
 import defaultAvatar from '../../assets/default-avatar.svg'
 import {
     DropdownMenu,
@@ -21,6 +22,16 @@ const BNav = React.forwardRef<
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => {
   const authState = useAuthedUserStore()
+
+  const onDropdownChange = (open: boolean) => {
+    if (!open) {
+      document.body.style.pointerEvents = ''
+    }
+  }
+
+  const logout = () => {
+    authState.logout()
+  }
 
   return (
     <div
@@ -35,19 +46,24 @@ const BNav = React.forwardRef<
         {SITE_NAME}
       </Link>
       <div className="flex items-center">
-        <Button size="sm" asChild className="mr-2">
+        <Button size="sm" asChild className="mr-4">
           <Link to="/submit">+ 提交</Link>
         </Button>
         {isLogined(authState) ? (
-          <DropdownMenu>
+          <DropdownMenu onOpenChange={onDropdownChange}>
             <DropdownMenuTrigger asChild>
-              <Avatar className="inline-block">
+              <Avatar className="inline-block cursor-pointer">
                 <AvatarImage src={defaultAvatar} />
                 <AvatarFallback>{authState.username}</AvatarFallback>
               </Avatar>
             </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuContent>退出</DropdownMenuContent>
+            <DropdownMenuContent className="px-0" align="end" sideOffset={8}>
+              <DropdownMenuItem
+                className="cursor-pointer py-1 px-2 hover:bg-gray-200 hover:outline-0"
+                onClick={logout}
+              >
+                退出
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         ) : (

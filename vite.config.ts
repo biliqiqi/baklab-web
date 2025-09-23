d48da3dcffe4e22617e81c14846aaf1ab4557ead
Kholin <oodzchen@gmail.com>
2025-09-23 11:20:57 +0800

    fix: vite config type error

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 // import { analyzer } from 'vite-bundle-analyzer'
 
